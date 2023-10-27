1684e74a4c28041526a84762a0503b02629110bb
Sanskar Modi <modisanskar5@gmail.com>
2023-10-28 01:26:15 +0530

    Initial Configuration

---
+++ android/app/src/main/kotlin/com/example/trando_assign/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trando_assign
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/assets.dart
@@ -0,0 +1,55 @@
+class HomePage {
+  static const homePath = 'assets/home_page';
+
+  static const coins = '$homePath/coins.png';
+  static const copy = '$homePath/copy.png';
+  static const courses = '$homePath/courses.png';
+  static const dailyLive = '$homePath/daily-live.png';
+  static const dailyNews = '$homePath/daily-news.png';
+  static const delhiPolice = '$homePath/delhi-police.png';
+  static const facebook = '$homePath/facebook.png';
+  static const gift = '$homePath/gift.png';
+  static const home = '$homePath/home.png';
+  static const insta = '$homePath/insta.png';
+  static const linkedin = '$homePath/linkedin.png';
+  static const ncert = '$homePath/ncert.png';
+  static const pyq = '$homePath/pyq.png';
+  static const schedule = '$homePath/schedule.png';
+  static const share = '$homePath/share.png';
+  static const syllabus = '$homePath/syllabus.png';
+  static const telegram = '$homePath/telegram.png';
+  static const tests = '$homePath/tests.png';
+  static const x = '$homePath/x.png';
+  static const yt = '$homePath/Youtube.png';
+  static const ytVideo = '$homePath/yt-video.png';
+}
+
+class Menu {
+  static const menuPath = 'assets/menu';
+
+  static const avatar = '$menuPath/avatar.png';
+  static const download = '$menuPath/download.png';
+  static const info = '$menuPath/info.png';
+  static const logout = '$menuPath/logout.png';
+  static const referEarn = '$menuPath/refer-earn.png';
+  static const resources = '$menuPath/resources.png';
+  static const settings = '$menuPath/settings.png';
+  static const share = '$menuPath/share.png';
+  static const support = '$menuPath/support.png';
+  static const wallet = '$menuPath/wallet.png';
+}
+
+class MyWallet {
+  static const walletPath = 'assets/my_wallet';
+
+  static const help = '$walletPath/help.png';
+  static const wallet = '$walletPath/wallet.png';
+}
+
+class ReferEarn {
+  static const referPath = 'assets/refer_earn';
+
+  static const campaign = '$referPath/campaign.png';
+  static const group = '$referPath/group.png';
+  static const whatsapp = '$referPath/whatsapp.png';
+}
